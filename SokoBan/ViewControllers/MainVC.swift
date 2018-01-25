18372904ebb72b3effbe393d1528645c6996b379
alexjjet <alexjjet@gmail.com>
2018-01-25 16:46:49 +0200

    add interface

---
+++ SokoBan/ViewControllers/MainVC.swift
@@ -0,0 +1,36 @@
+//
+//  MainVC.swift
+//  SokoBan
+//
+//  Created by Alexandr Kukushkin on 25.01.2018.
+//  Copyright © 2018 Alexandr Kukushkin. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+    @IBOutlet weak var levelLabel: UILabel!
+    
+    @IBOutlet weak var stepsLabel: UILabel!
+    
+    @IBOutlet weak var drawLevelLabel: UILabel!
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    @IBAction func upButton(_ sender: UIButton) {
+    }
+    @IBAction func downButton(_ sender: UIButton) {
+    }
+    @IBAction func rightButton(_ sender: UIButton) {
+    }
+    @IBAction func leftButton(_ sender: UIButton) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+}
+
